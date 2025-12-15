bf9df2f954f1299a2655f582b0f675155ab99915
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-16 02:52:13 +0300

    bitplru done (firstly did tree-lru which was wrong)

--- include/cache_bplru.hpp
+++ include/cache_bplru.hpp
@@ -8,13 +8,12 @@ public:
     explicit CacheBpLRU(RAM& ram);
 
 private:
-    void reset_policy() override;
     uint32_t choose_victim(uint32_t set) override;
     void on_hit(uint32_t set, uint32_t way) override;
     void on_fill(uint32_t set, uint32_t way) override;
 
 private:
-    uint8_t plru_bits[CACHE_SET_COUNT]; // 3 бита на сет для 4-way
+    uint8_t used[CACHE_SET_COUNT][CACHE_WAY];
 };
 
 #endif // CACHE_BPLRU_HPP
--- src/cache_bplru.cpp
+++ src/cache_bplru.cpp
@@ -1,46 +1,38 @@
 #include "cache_bplru.hpp"
+#include <cstring>
 
 CacheBpLRU::CacheBpLRU(RAM& ram) : CacheAbstract(ram) {
-    reset_policy();
+    std::memset(used, 0, sizeof(used));
 }
 
-// сброс бит → все линии «левые» на старте
-void CacheBpLRU::reset_policy() {
-    std::memset(plru_bits, 0, sizeof(plru_bits));
-}
-
-// выбор линии-жертвы по битам
 uint32_t CacheBpLRU::choose_victim(uint32_t set) {
-    uint8_t bits = plru_bits[set];
-    uint32_t way = 0;
-
-    // b0
-    if (bits & 0b100) { // правая ветка
-        way |= 0b10;
-    }
-    // b1 или b2
-    if (bits & 0b010) { // b1, левая правая
-        way |= 0b01; // w1
-    } else if (bits & 0b001) { // b2, правая ветка
-        way |= 0b11; // w3
+    for (uint32_t way = 0; way < CACHE_WAY; ++way) {
+        if (used[set][way] == 0)
+            return way;
     }
 
-    return way & 0x3; // 0..3
+    return 0;
 }
 
-// обновление бит при hit
 void CacheBpLRU::on_hit(uint32_t set, uint32_t way) {
-    uint8_t& bits = plru_bits[set];
+    used[set][way] = 1;
 
-    switch (way) {
-        case 0: bits &= ~0b100; bits &= ~0b010; break; // w0
-        case 1: bits &= ~0b100; bits |=  0b010; break; // w1
-        case 2: bits |=  0b100; bits &= ~0b001; break; // w2
-        case 3: bits |=  0b100; bits |=  0b001; break; // w3
+    bool all_used = true;
+    for (uint32_t w = 0; w < CACHE_WAY; ++w) {
+        if (used[set][w] == 0) {
+            all_used = false;
+            break;
+        }
+    }
+
+    if (all_used) {
+        for (uint32_t w = 0; w < CACHE_WAY; ++w)
+            used[set][w] = 0;
+
+        used[set][way] = 1;
     }
 }
 
-// при fill — ведем себя так же, как hit
 void CacheBpLRU::on_fill(uint32_t set, uint32_t way) {
     on_hit(set, way);
 }
