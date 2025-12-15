f9950c0124a99b2b8dc10b2af1d67d549ce3da1f
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-16 02:49:15 +0300

    cache lru fixed

--- include/cache_lru.hpp
+++ include/cache_lru.hpp
@@ -6,16 +6,14 @@
 class CacheLRU : public CacheAbstract {
 public:
     explicit CacheLRU(RAM& ram);
-    
+
 private:
-    void reset_policy() override;
     uint32_t choose_victim(uint32_t set) override;
     void on_hit(uint32_t set, uint32_t way) override;
     void on_fill(uint32_t set, uint32_t way) override;
 
 private:
-    // для каждого сета храним массив "возраста" для 4 путей
-    uint8_t lru_age[CACHE_SET_COUNT][CACHE_WAY];
+    uint8_t last_used[CACHE_SET_COUNT][CACHE_WAY];
 };
 
-#endif // CACHER_LRU_HPP
+#endif // CACHE_LRU_HPP
